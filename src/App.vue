a8d5b388f31a3dd404ab07cc1d84518f2c05d2ce
Salih Gumus <sudanmerinosu@gmail.com>
2022-04-02 12:01:45 +0300

    [feature][Salih] add variables and styles

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Chat msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Chat from './components/ChatBox.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Chat,
   },
 };
 </script>
 
 <style>
+:root {
+  --blue: #007fef;
+  --borderRadiusBase: 3px;
+  --chatWidth: 48rem;
+  --chatInputWidth: 48rem;
+  --chatBackground: #eee;
+  --chatInputHeight: 5rem;
+  --chatInputInnerHeight: 3rem;
+}
+* {
+  box-sizing: border-box;
+}
+
+body {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  background: var(--blue);
+  font-family: 'Roboto', sans-serif;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
